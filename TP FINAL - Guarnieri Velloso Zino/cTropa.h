ebc7d54ad92409d0c221fb5c3dbb948223f41f0d
Manu Velloso <manuvelloso2@gmail.com>
2021-06-01 21:15:15 -0300

    .h cTropa

--- TP FINAL - Guarnieri Velloso Zino/cTropa.h
+++ TP FINAL - Guarnieri Velloso Zino/cTropa.h
@@ -9,21 +9,21 @@
 #define EA_48AC6C59_7B06_45a6_B915_81B52F35D200__INCLUDED_
 
 #include "cGuerrero.h"
+#include "cListaT.h"
 
 class cTropa
 {
+private:
+	unsigned int AT_Total;
+	int NumTropa;
 
 public:
 	cTropa();
 	virtual ~cTropa();
-	cGuerrero *m_cGuerrero;
+	cListaT<cGuerrero> Guerreros;
 
 	unsigned int CalcularAT_Total();
 	void RecibirDanio(unsigned int AT_Ataque);
-
-private:
-	unsigned int AT_Total;
-	int NumTropa;
 
 };
 #endif // !defined(EA_48AC6C59_7B06_45a6_B915_81B52F35D200__INCLUDED_)

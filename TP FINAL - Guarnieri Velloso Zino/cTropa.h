1547bea656528e7e2835a61815e1c63d01a980f8
Manu Velloso <manuvelloso2@gmail.com>
2021-06-01 21:18:15 -0300

    cambios en atributos privados

--- TP FINAL - Guarnieri Velloso Zino/cTropa.h
+++ TP FINAL - Guarnieri Velloso Zino/cTropa.h
@@ -16,14 +16,13 @@ class cTropa
 private:
 	unsigned int AT_Total;
 	int NumTropa;
+	cListaT<cGuerrero> Guerreros;
 
 public:
 	cTropa();
 	~cTropa();
-	cListaT<cGuerrero> Guerreros;
 
 	unsigned int CalcularAT_Total();
 	void RecibirDanio(unsigned int AT_Ataque);
-
 };
 #endif // !defined(EA_48AC6C59_7B06_45a6_B915_81B52F35D200__INCLUDED_)

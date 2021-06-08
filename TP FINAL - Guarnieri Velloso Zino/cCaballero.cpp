1ddbc35cd174713cac518dc47f8fbb32051db500
Manu Velloso <manuvelloso2@gmail.com>
2021-06-07 21:02:17 -0300

    constructor cCaballero + comentarios

--- TP FINAL - Guarnieri Velloso Zino/cCaballero.cpp
+++ TP FINAL - Guarnieri Velloso Zino/cCaballero.cpp
@@ -7,44 +7,24 @@
 
 #include "cCaballero.h"
 
-
-cCaballero::cCaballero(unsigned int _AT, unsigned int _AT_ESPECIAL, unsigned int _HP_ACTUAL, unsigned int _HP_INICIAL, unsigned int _LimiteVida):cGuerrero(_AT,_AT_ESPECIAL,_HP_ACTUAL,_HP_INICIAL,_LimiteVida){
+cCaballero::cCaballero():cGuerrero((rand() % 11) + 30, (rand() % 21) + 80, (rand() % 21) + 80){
 	//Los caballeros tienen un ataque (AT) entre 30-40 y entre 80 y 100 de vida(HP).
-	HP_Inicial = (rand() % 21) + 80;
-	HP_Actual = HP_Inicial;
-	AT = (rand() % 11) + 30;
-	AT_Especial = AT;
-	LimiteVida = HP_Inicial - (0.05 * HP_Inicial);
 }
-
-
 
 cCaballero::~cCaballero(){
 
 }
-
-
-
-
 
 unsigned int cCaballero::CondicionAtaque(cGuerrero* Atacado){
 
 	return 0;
 }
 
-
 void cCaballero::Contrataque(){
 
 }
 
-
 unsigned int cCaballero::GenerarAT(){
-
-	return 0;
-}
-
-
-unsigned int cCaballero::GenerarHP(){
 
 	return 0;
 }
--- TP FINAL - Guarnieri Velloso Zino/cCaballero.h
+++ TP FINAL - Guarnieri Velloso Zino/cCaballero.h
@@ -19,20 +19,38 @@ private:
 
 public:
 #pragma region CONSTRUCTORES & DESTRUCTORES
-
-	cCaballero(unsigned int _AT, unsigned int _HP_ACTUAL, unsigned int _HP_INICIAL);
+	/**
+	* cCaballero
+	* constructor de la clase cCaballero
+	*/
+	cCaballero();
+	/**
+	* ~cCaballero
+	* destructor de la clase cCaballero
+	*/
 	~cCaballero();
 
 #pragma endregion
 
 #pragma region GENERADORES
-
+	/**
+	* GenerarAT
+	* devuelve un numero random entre .Este es la fuerza de ataque de una sola unidad
+	* @return unsigned int
+	*/
 	unsigned int GenerarAT();
 
 #pragma endregion
-
+	/**
+	* CondicionAtaque
+	* evalua si es fuerte contra el otro -> Caballero es fuerte contra el arquero
+	* @param cGuerrro*
+	* @return int
+	*/
 	unsigned int CondicionAtaque(cGuerrero* Atacado);
+	/**
+	* Contrataque //TODO: terminar comentario
+	*/
 	void Contrataque();
-
 };
 #endif // !defined(EA_9AC6C449_ED7E_4e46_8F84_741DAFDA5EC4__INCLUDED_)

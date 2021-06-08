e26004c2aba6ee9096ad269936db742be17a9e7c
Manu Velloso <manuvelloso2@gmail.com>
2021-06-07 21:50:19 -0300

    comentarios cMundo

--- TP FINAL - Guarnieri Velloso Zino/cMundo.h
+++ TP FINAL - Guarnieri Velloso Zino/cMundo.h
@@ -7,25 +7,51 @@
 class cMundo
 {
 private:
-	cListaT<cPais>* ListaPaises;
+	cListaT<cPais>* ListaPaises; //lista de paises que constituyen al mundo
 
 #pragma region METODOS DE ARCHIVOS
-
+	/**
+	* LeerArchivo 
+	* abre el .txt correspondiente al mundo elegido en el main. Lee linea por linea,
+	* separando los nombres (separados por '-') y crea la lista de strings que tiene los nombres de los paises limitrofes
+	* @param filename
+	*/
 	void LeerArchivo(const char* Filename);
 
 #pragma endregion
 	
 public:
 #pragma region CONSTRUCTORES & DESTRUCTORES
-
-	cMundo(); //esta lista sale del archivo
+	/**
+	* cMundo
+	* constructor de la clase cMundo
+	*/
+	cMundo();
+	/**
+	* cMundo
+	* constructor de la clase cMundo
+	*/
 	~cMundo();
 #pragma endregion
 
 #pragma region SET UP
-
-	void PaisesLimitrofes(string& Limitrofes, string NombrePais); //Crear array de strings con nombres de paises limitrofes PARA 1 SOLO PAIS
+	/**
+	* PaisesLimitrofes 
+	* Crear array de strings con nombres de paises limitrofes PARA 1 SOLO PAIS
+	* @param string, nombre 
+	*/
+	void PaisesLimitrofes(string& Limitrofes, string NombrePais);
+	/**
+	* BuscarNombrePais
+	* Por medio de funciones de la class string, se buscar el nombre del pais
+	* @param string, caracter, posicion
+	*/
 	string BuscarNombrePais(string str, char separator, int* pos);
+	/**
+	* SetUp
+	* inicializa al mundo
+	* @param filename
+	*/
 	void SetUp(const char* Filename);
 
 #pragma endregion

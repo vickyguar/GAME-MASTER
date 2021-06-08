27eacd3e973bb744c3b03a164c856f99b55856ba
Manu Velloso <manuvelloso2@gmail.com>
2021-06-07 21:08:27 -0300

    constructor mago + comentarios

--- TP FINAL - Guarnieri Velloso Zino/cMago.cpp
+++ TP FINAL - Guarnieri Velloso Zino/cMago.cpp
@@ -8,24 +8,13 @@
 #include "cMago.h"
 
 
-cMago::cMago(unsigned int _AT, unsigned int _AT_ESPECIAL, unsigned int _HP_ACTUAL, unsigned int _HP_INICIAL, unsigned int _LimiteVida):cGuerrero(_AT, _AT_ESPECIAL, _HP_ACTUAL, _HP_INICIAL, _LimiteVida){
+cMago::cMago():cGuerrero((rand() % 11) + 10, (rand() % 21) + 40, (rand() % 21) + 40){
 	//10 - 20 y entre 40 y 60 de vida
-	AT = (rand() % 11) + 10;
-	AT_Especial = AT;
-	HP_Inicial = (rand() % 21) + 40;
-	HP_Actual = HP_Inicial;
-	LimiteVida = HP_Inicial - (0.05 * HP_Inicial);
 }
-
-
 
 cMago::~cMago(){
 
 }
-
-
-
-
 
 /**
  * Lo que dijo el Pampa: reparte su ataque en toda la tropa
@@ -35,14 +24,7 @@ unsigned int cMago::CondicionAtaque(cGuerrero* Atacado){
 	return 0;
 }
 
-
 unsigned int cMago::GenerarAT(){
-
-	return 0;
-}
-
-
-unsigned int cMago::GenerarHP(){
 
 	return 0;
 }
--- TP FINAL - Guarnieri Velloso Zino/cMago.h
+++ TP FINAL - Guarnieri Velloso Zino/cMago.h
@@ -16,14 +16,17 @@ class cMago : public cGuerrero
 public:
 #pragma region CONSTRUCTORES & DESTRUCTORES
 
-	cMago(unsigned int _AT, unsigned int _HP_ACTUAL, unsigned int _HP_INICIAL);
+	cMago();
 	~cMago();
 
 #pragma endregion
 
-	
-
 	unsigned int CondicionAtaque(cGuerrero* Atacado);
+	/**
+	* GenerarAT
+	* devuelve un numero random entre .Este es la fuerza de ataque de una sola unidad
+	* @return unsigned int
+	*/
 	unsigned int GenerarAT();
 
 };
